bf10eec31916c046795023a14c6b5151c776a71d
Ra6nar0k21 | Michael <38357618+michaeltrosin@users.noreply.github.com>
2021-05-11 10:01:25 +0200

    Added file argument

--- main.cpp
+++ main.cpp
@@ -3,21 +3,25 @@
 //
 
 #include "BrainfuckInterpreter.h"
+
 #include <fstream>
 #include <iostream>
 #include <streambuf>
 #include <string>
 
-int main(int argc, char** argv){
-    std::ifstream t("../test.bf");
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << "Missing file argument" << std::endl;
+        return 1;
+    }
+    std::ifstream t(argv[1]);
     std::string str;
 
     t.seekg(0, std::ios::end);
     str.reserve(t.tellg());
     t.seekg(0, std::ios::beg);
 
-    str.assign((std::istreambuf_iterator<char>(t)),
-               std::istreambuf_iterator<char>());
+    str.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 
     BrainfuckInterpreter interpreter(str);
     interpreter.run();
